6fcc1903714a9293b9576eebc8f90ec8b8cbc349
gabriel-dehan <dehan.gabriel@gmail.com>
2014-04-01 01:18:20 +0200

    Fixes a slight bug with options and updates the readme

--- app/kittcat/jquery.kittcat.js
+++ app/kittcat/jquery.kittcat.js
@@ -3,7 +3,7 @@ $.fn.kittcat = function(opts) {
     this.attr('ng-controller', 'KittcatController');
     this.html(document.templates['container']);
 
-    var api_host = opts.api || document.host,
+    var api_host = opts.api || document.location.host,
         endpoint = opts.endpoint || 'data',
         file_endpoint = opts.fileEndpoint || '/file'
         tree_endpoint = opts.treeEndpoint || '/tree';
